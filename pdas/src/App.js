6609890342140cc08d7edbc056d518c838e000c1
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2022-11-27 17:35:38 +0530

    pdas complete project

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -6,12 +6,29 @@ import ProfileCard from './ProfileCard';
 
 function App() {
   return (
+    <div>
+    <section className="hero is-primary">
+    <div className="hero-body">
+    <p className="title"> Personal Digital Assistants</p>
+    </div>
+    </section>
     <div className="container">
+    <section className="section">
+    <div className="columns">
+    <div className="column is-3">
+      <ProfileCard title="Alexa" handle="@alexa99" image={Alexa}/>
+      </div>
     
-    <div>Personal Digital Assitants</div>
-    <ProfileCard title="Alexa" handle="@alexa99" image={Alexa}/>
+  <div className="column is-3">
     <ProfileCard title="cortana" handle="@cortana32" image={cortana}/>
+    </div>
+
+    <div className="column is-3">
     <ProfileCard title="siri" handle="@siri01" image={siri}/>
+    </div>
+    </div>
+    </section>
+    </div>
     </div>
   );
   }
@@ -22,4 +39,12 @@ export default App;
    // <img src ={siri}/>
 
 
-
+{/* 
+//<div className="container">
+ <section className="section"> 
+ <div className="columns">
+  <div className="column is-4">
+   <ProfileCard title="Alexa" handle="@alexa99" image={Alexa Image} </div> <div className="column is-4"> 
+   <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} /> 
+   </div> <div className="column s-4">
+ <ProfileCard title="Siri" handle="@siri01" image={SiriImage} /> */}
