50340ee135f29b16e53edf22f7390dcf54656e23
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2022-11-29 23:02:03 +0530

    animals project

--- animals/animals/src/App.js
+++ animals/animals/src/App.js
@@ -1,8 +1,25 @@
 /** @format */
-
+// array destructing
+//function makearray()
+//return [1, 2, 34, 4];
+// const [firstelement,secondelement] =makearray();
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+function getrandomanimal() {
+	const animals = ["bird", "cat", "cow", "dog", "alligator", "horse"];
+	return animals[Math.floor(Math.random() * animals.length)];
+}
 function App() {
+	// const [count, setCount] = useState(0);
+	const [animals, setAnimals] = useState([]);
+
 	const handleclick = () => {
-		console.log("button was clicked");
+		setAnimals([...animals, getrandomanimal()]);
+
+		// console.log("button was clicked");
+		// const renderedAnimals =animals.map((animal,index) => {
+		//   return <AnimalShow type ={animal} key={index}/>
+		// };
 	};
 	return (
 		<div>
